3e43d23741481b1bfe23056f2acd1db333d0d144
Shadi Akiki <shadiakiki1986@gmail.com>
2020-01-06 12:51:03 +0200

    version bump 0.1.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(fname):
 # and https://packaging.python.org/tutorials/packaging-projects/
 setup(
     name='lsvine',
-    version='0.1.1',
+    version='0.1.2',
     author="Shadi Akiki, AutofitCloud",
     author_email="shadi@autofitcloud.com",
     url='https://github.com/autofitcloud/lsvine',
