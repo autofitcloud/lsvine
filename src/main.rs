77b795adf6edecad6c24aaf2ba881559fc14a910
Shadi Akiki <shadiakiki1986@gmail.com>
2020-01-07 16:49:28 +0200

    feat: break down single table of all directories into several tables each of which fits on the terminal width

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,8 @@ use prettytable::{Table, Row, Cell};
 use prettytable::format;
 
 // https://eminence.github.io/terminal-size/doc/terminal_size/index.html
-use terminal_size::{Width, Height, terminal_size};
+// use terminal_size::{Width, Height, terminal_size};
+use termion::terminal_size;
 
 // https://doc.rust-lang.org/std/cmp/fn.min.html
 use std::cmp;
@@ -136,6 +137,29 @@ fn main() -> io::Result<()> {
 
     }
 
+    // get maximum file length per row in table1, in cumulative sum .. no longer in cum sum
+    // TODO Isn't this stuff that used to come from pandas/numpy in python?
+    // Need to factor out into a library or find something like
+    // (example) https://docs.rs/ndarray/0.13.0/ndarray/index.html
+    // (old)     https://www.reddit.com/r/rust/comments/5ks00y/introducing_utah_a_rust_dataframe/
+    // (new)     https://www.reddit.com/r/rust/comments/apo66e/dataframes_what_do_you_need/
+    let mut max_all: Vec<usize> = Vec::new();
+    for i in 0..table1.len() {
+      let mut max_cur = dirnames[i].chars().count(); // initialize to column header
+      //println!("max filename length in {} = {} ...", dirnames[i], max_cur);
+
+      for j in 0..table1[i].len() {
+        let max_cell = table1[i][j].get_content().chars().count();
+        max_cur = cmp::max(max_cur, max_cell);
+        //println!("new filename {} \t\t max cur {} \t max cell {}", table1[i][j].get_content(), max_cur, max_cell);
+      }
+      // let max_pre = if max_all.len()==0 { 0 } else { max_all[max_all.len()-1] };
+      //println!("max pre {}, max cur {}", max_pre, max_cur);
+      // max_all.push(max_pre + max_cur + 3); // always add 3 characters
+      max_all.push(max_cur);
+    }
+    //println!("{:?}", max_all);
+
     // print table
     // table1.printstd();
 
@@ -153,12 +177,9 @@ fn main() -> io::Result<()> {
       process::exit(2);
     }
 
-    // get terminal width
-    let size = terminal_size();
-    let mut _terminal_width = 0;
-    if let Some((Width(w), Height(_h))) = size {
-        _terminal_width = w;
-    }
+    // get terminal width .. surely there is a better way
+    let _terminal_width = terminal_size().unwrap().0 as usize;
+    // println!("terminal width 1 {}", _terminal_width);
 
     // transpose the table
     let mut table2 = Table::new();
@@ -166,17 +187,36 @@ fn main() -> io::Result<()> {
 
     // number of tables displayed
     let mut idx_table = 0;
-    let max_col = 5;
+    // maximum number of columns, will override later when first table is printed
+    let mut max_col = 5;
+    // cumulative sum of width
+    let mut max_cum = 0;
+    let mut sum_displayed = 0;
     for i in 0..nrow+1 {
       // debug
-      // println!("i {}, idx_table {}, dirnames.len {}", i, idx_table, dirnames.len());
-    
+      //println!("i {}, nrow {}, idx_table {}, dirnames.len {}", i, nrow, idx_table, dirnames.len());
+   
+      // add
+      if i<nrow {
+        max_cum = max_cum + max_all[i] + 3; // add 3 characters
+        //println!("max cum {}, term wid {}", max_cum, _terminal_width);
+      }
+ 
       // if need to flush current table2
-      if i==nrow || (i > 0 && i % max_col == 0) {
+      // if i==nrow || (i > 0 && i % max_col == 0) {
+      if i==nrow || max_cum >= _terminal_width {
+        // reset
+        if i<nrow {
+          max_cum = max_all[i];
+        }
+
+        // override max_col
+        max_col = i - sum_displayed; // i doesnt work because after the first wrap, need to subtract the sum of columns displayed so far // table2.len() is the number of rows .. we need number of columns here
+        //println!("max col {}, nrow {}", max_col, nrow);
+
         // set title: https://crates.io/crates/prettytable-rs
         // slicing // let row_titles = l1[idx_table*max_col .. (idx_table+1)*max_col-1].to_vec().iter().map(|res| Cell::new(res.file_name().unwrap().to_str().unwrap())).collect();
-        let idx_end = cmp::min(nrow, (idx_table+1)*max_col-1);
-        let row_titles = dirnames[idx_table*max_col .. idx_end].to_vec().iter().map(|res| Cell::new(res)).collect();
+        let row_titles = dirnames[sum_displayed .. sum_displayed + max_col].to_vec().iter().map(|res| Cell::new(res)).collect();
         table2.set_titles(Row::new(row_titles));
 
         // print table
@@ -188,6 +228,7 @@ fn main() -> io::Result<()> {
 
         // increment
         idx_table = idx_table + 1;
+        sum_displayed = sum_displayed + max_col;
       }
 
       if i==nrow {
@@ -195,6 +236,8 @@ fn main() -> io::Result<()> {
       }
 
       let ncol = table1[i].len();
+      //println!("add col, {}, n files {}", dirnames[i], ncol);
+
       if ncol==0 {
         continue
       }
@@ -210,7 +253,7 @@ fn main() -> io::Result<()> {
           }
         }
 
-        // println!("Table2 {} {}", table2.len(), table2[j].len());
+        //println!("Table2 {} {}", table2.len(), table2[j].len());
         table2[j].add_cell(Cell::new(table1[i][j].get_content().as_str()));
       }
     }
