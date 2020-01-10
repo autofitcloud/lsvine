c6d0bda79705f43c809b7e483f2c778d2879fedd
Shadi Akiki <shadiakiki1986@gmail.com>
2020-01-10 09:02:27 +0200

    enh: clean up testing

--- tests/vecpath2vecl1dir_iterators.rs
+++ tests/vecpath2vecl1dir_iterators.rs
@@ -18,30 +18,87 @@ pub mod vecpath2vecl1dir_iterators;
 use vecpath2vecl1dir_iterators::{PathBufWrap, RDAdapter1, RDAdapter2};
 use level1dir::Level1Dir;
 
+// File or dir, for use in create_dirstructure
+// https://doc.rust-lang.org/rust-by-example/custom_types/enum.html
+// https://stackoverflow.com/questions/40559931/vector-store-mixed-types-of-data-in-rust
+pub enum FoD {
+  F(i32),
+  H(i32), // hidden file (starts with .)
+  D(Vec<FoD>)
+}
+
+use std::fmt;
+impl fmt::Display for FoD {
+  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+      match self {
+        FoD::F(n_files) => {
+          write!(f, "f{}", n_files)
+        },
+        FoD::H(n_files) => {
+          write!(f, "h{}", n_files)
+        },
+        FoD::D(vec_files_or_dirs) => {
+          write!(f, "d{}", vec_files_or_dirs.len())
+        }
+      }
+  }
+}
 
 // utility function for tests
-pub fn _create_vecpath_twofiles_onedironefile(dir_1: &tempfile::TempDir) -> Result<Vec<std::path::PathBuf>, io::Error> {
-    // a dir with 2 files
+// root_nfiles: number of files in the root dir
+// dir_struct: list of int or list describing the structure expected
+//             eg [2, [0,2]] means 2 files in root, 1 dir in root, 0 files in first dir in root, 2 dirs in first dir in root
+pub fn create_dirstructure(rootdir_path: &std::path::Path, dir_struct: &Vec<FoD>, level_opt: Option<i32>, input_opt: Option<Vec<std::path::PathBuf>>) -> Result<Vec<std::path::PathBuf>, io::Error> {
+    // http://stackoverflow.com/questions/24047686/ddg#35369909
+    // note that this doesn't get passed into the recursion, hence only contains first-level data
+    let mut input_vec = input_opt.unwrap_or(Vec::new());
 
-    let file_path_1 = dir_1.path().join("my-temporary-note.txt");
-    File::create(&file_path_1)?;
-    let file_path_2 = dir_1.path().join(".hidden.txt");
-    File::create(&file_path_2)?;
-    let file_path_2 = dir_1.path().join("another-note.txt");
-    File::create(&file_path_2)?;
-    let dir_path_2 = dir_1.path().join("subdir");
-    create_dir(&dir_path_2)?;
-    let file_path_3 = dir_path_2.join("some-pic.txt");
-    File::create(&file_path_3)?;
+    let level_int = level_opt.unwrap_or(0);
 
-    let mut input = Vec::new();
-    input.push(file_path_1);
-    input.push(file_path_2);
-    input.push(dir_path_2);
-    // no need to insert this, the function will traverse again and find it
-    // input.push(file_path_3.to_path_buf());
+    println!("cretae_direstruct {}: enter", level_int);
 
-    Ok(input)
+    for (j, dir_content) in dir_struct.iter().enumerate() {
+      // https://doc.rust-lang.org/rust-by-example/flow_control/if_let.html
+      let mut prefix = "";
+      if let FoD::H(_n_files) = dir_content { prefix = "."; }
+      println!("level {}, dirconten {}, prefix {}", level_int, dir_content, prefix);
+
+      match dir_content {
+        FoD::F(n_files) | FoD::H(n_files) => {
+          for i in 0..*n_files {
+            let file_path = rootdir_path.join(format!("{}f_{}_{}_{}.txt", prefix, level_int, j, i));
+            File::create(&file_path)?; // raise on error
+            if prefix != "." {
+              input_vec.push(file_path);
+            }
+          }
+        },
+        FoD::D(vec_files_or_dirs) => {
+          let subdir_path = rootdir_path.join(format!("d_{}_{}", level_int, j));
+          create_dir(&subdir_path)?; // raise on error
+          input_vec.push(subdir_path);
+          let _ = create_dirstructure(&input_vec.last().unwrap(), &vec_files_or_dirs, Some(level_int+1), None);
+        }
+      }
+    }
+
+    println!("cretae_direstruct {}: leave", level_int);
+
+    Ok(input_vec)
+}
+
+
+// utility variable
+fn dirstruct_f2_d1() -> Vec<FoD> { vec![FoD::F(2), FoD::H(1), FoD::D(vec![FoD::H(1), FoD::F(1)])] }
+
+#[test]
+fn test_create_dirstructure() -> io::Result<()> {
+    // Need to return Result in order to benefit from "?" after tempdir() below
+    let dir_1 = tempfile::tempdir()?;
+    let input_vec = create_dirstructure(dir_1.path(), &dirstruct_f2_d1(), None, None);
+    assert_eq!(input_vec.is_ok(), true);
+    assert_eq!(input_vec.unwrap().len(), 3);
+    Ok(())
 }
 
 
@@ -49,7 +106,7 @@ pub fn _create_vecpath_twofiles_onedironefile(dir_1: &tempfile::TempDir) -> Resu
 fn transform_readdir() -> io::Result<()> {
     // Note about "?" below: failures to create tempdir/files are beyond the scope of this package, so it's ok
     let dir_1 = tempfile::tempdir()?;
-    _create_vecpath_twofiles_onedironefile(&dir_1)?;
+    create_dirstructure(&dir_1.path(), &dirstruct_f2_d1(), None, None)?;
     let fs_readdir = std::fs::read_dir(dir_1.path())?;
 
     // get iterator
@@ -78,7 +135,8 @@ fn transform_readdir() -> io::Result<()> {
 fn rdadapter1_direct() -> io::Result<()> {
     // Note about "?" below: failures to create tempdir/files are beyond the scope of this package, so it's ok
     let dir_1 = tempfile::tempdir()?;
-    _create_vecpath_twofiles_onedironefile(&dir_1)?;
+    create_dirstructure(&dir_1.path(), &dirstruct_f2_d1(), None, None)?;
+
     let fs_readdir = std::fs::read_dir(dir_1.path())?;
 
     // Get iterator
@@ -97,7 +155,8 @@ fn rdadapter1_direct() -> io::Result<()> {
 fn rdadapter1_recur() -> io::Result<()> {
     // Note about "?" below: failures to create tempdir/files are beyond the scope of this package, so it's ok
     let dir_1 = tempfile::tempdir()?;
-    _create_vecpath_twofiles_onedironefile(&dir_1)?;
+    create_dirstructure(&dir_1.path(), &dirstruct_f2_d1(), None, None)?;
+
     let fs_readdir = std::fs::read_dir(dir_1.path())?;
 
     // Get iterator then split file versus dir
@@ -146,7 +205,7 @@ fn rdadapter1_recur() -> io::Result<()> {
 fn rdadapter2() -> io::Result<()> {
     // Note about "?" below: failures to create tempdir/files are beyond the scope of this package, so it's ok
     let dir_1 = tempfile::tempdir()?;
-    _create_vecpath_twofiles_onedironefile(&dir_1)?;
+    create_dirstructure(&dir_1.path(), &dirstruct_f2_d1(), None, None)?;
 
     // Get iterator
     // Need to consume it ONCE otherwise will get error[E0382]: use of moved value: `rda1_all`
